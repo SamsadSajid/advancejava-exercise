39f4d925547929a93586cc04f1916c0afdafa8a1
Sajid Samsad <1405118.ssr@ugrad.cse.buet.ac.bd>
2020-08-22 14:18:50 +0600

    Implement fibonacci with stream api

--- src/main/java/com/masterdevskills/cha2/ext1/NumericStreams.java
+++ src/main/java/com/masterdevskills/cha2/ext1/NumericStreams.java
@@ -22,10 +22,12 @@
 
 package com.masterdevskills.cha2.ext1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -45,6 +47,9 @@ public class NumericStreams {
 	 */
 	public static List<Integer> generate(int series) {
 
-		throw new RuntimeException("TODO://ImplementIt");
+		return Stream.iterate(new int[] {0, 1}, n -> new int[] {n[1], n[0] + n[1]})
+				.limit(series)
+				.map(n -> n[1])
+				.collect(Collectors.toList());
 	}
 }
